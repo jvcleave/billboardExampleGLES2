20671cd30d76dfb0efd9db1be97343b240a66db3
Jason Van Cleave <jvcleave@gmail.com>
2013-01-20 04:12:51 -0500

    removing obj

--- obj/linuxarmv6l/Releaseaddons/ofxXmlSettings/libs/tinyxml/src/tinyxmlerror.d
+++
@@ -1,5 +0,0 @@
-obj/linuxarmv6l/Releaseaddons/ofxXmlSettings/libs/tinyxml/src/tinyxmlerror.o: \
- ../../../addons/ofxXmlSettings/libs/tinyxml/src/tinyxmlerror.cpp \
- ../../../addons/ofxXmlSettings/libs/tinyxml/src/tinyxml.h
-
-../../../addons/ofxXmlSettings/libs/tinyxml/src/tinyxml.h:
--- obj/linuxarmv6l/Releaseaddons/ofxXmlSettings/libs/tinyxml/src/tinyxmlparser.d
+++
@@ -1,5 +0,0 @@
-obj/linuxarmv6l/Releaseaddons/ofxXmlSettings/libs/tinyxml/src/tinyxmlparser.o: \
- ../../../addons/ofxXmlSettings/libs/tinyxml/src/tinyxmlparser.cpp \
- ../../../addons/ofxXmlSettings/libs/tinyxml/src/tinyxml.h
-
-../../../addons/ofxXmlSettings/libs/tinyxml/src/tinyxml.h:
